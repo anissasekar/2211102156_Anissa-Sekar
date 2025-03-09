72b0c80f34f2ca6b7936ec9b09e4e997d2b1070d
anissasekar <anissasekar@student.telkomuniversity.ac.id>
2025-03-09 15:51:09 +0700

    praktikum_3

---
+++ praktikum_3/lib/listviewseparated.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> entries = <String>['A', 'B', 'C'];
+    final List<int> colorCodes = <int>[600, 500, 100];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView Separated Example'),
+        ),
+        body: ListView.separated(
+          padding: const EdgeInsets.all(8),
+          itemCount: entries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 50,
+              color: Colors.amber[colorCodes[index]],
+              child: Center(child: Text('Entry ${entries[index]}')),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) => const Divider(),
+        ),
+      ),
+    );
+  }
+}
--- tugas2_2/android/app/src/main/kotlin/com/example/tugas2_2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tugas2_2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
